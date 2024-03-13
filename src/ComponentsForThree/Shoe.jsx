74bd0db47b17554649c90f0cba059fe072a5b449
real <hunkfuty@gamil.com>
2024-03-13 10:22:58 +0900

    choe:useFrame reset at Shoe

--- src/ComponentsForThree/Shoe.jsx
+++ src/ComponentsForThree/Shoe.jsx
@@ -9,26 +9,28 @@ import { useSnapshot } from "valtio"
 export function Shoe() {
     const ref = useRef()
     const snap = useSnapshot(state)
+    // console.log(part)
     const { nodes, materials } = useGLTF("shoe-draco.glb")
-    useFrame((state) => {
+    useFrame((state, delta) => {
       const t = state.clock.getElapsedTime()
       ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20)
       ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
-    })
-    useFrame((state, delta) => easing.dampC(materials.laces.color, snap.items.laces, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.mesh.color, snap.items.mesh, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.caps.color, snap.items.caps, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.inner.color, snap.items.inner, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.sole.color, snap.items.sole, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.stripes.color, snap.items.stripes, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.band.color, snap.items.band, 0.3, delta))
-    useFrame((state, delta) => easing.dampC(materials.patch.color, snap.items.patch, 0.3, delta))
-  
+    //   easing.dampC(materials.part.color, snap.items.part, 0.3, delta)
+    easing.dampC(materials.laces.color, snap.items.laces, 0.3, delta)
+    easing.dampC(materials.mesh.color, snap.items.mesh, 0.3, delta)
+    easing.dampC(materials.caps.color, snap.items.caps, 0.3, delta)
+    easing.dampC(materials.inner.color, snap.items.inner, 0.3, delta)
+    easing.dampC(materials.sole.color, snap.items.sole, 0.3, delta)
+    easing.dampC(materials.stripes.color, snap.items.stripes, 0.3, delta)
+    easing.dampC(materials.band.color, snap.items.band, 0.3, delta)
+    easing.dampC(materials.patch.color, snap.items.patch, 0.3, delta)
+})
+   
     return (
       <group
         ref={ref}
         onPointerMissed={() => (state.current = null)}
-        onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name), console.log(state.current))}>
+        onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
         <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
         <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
         <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
